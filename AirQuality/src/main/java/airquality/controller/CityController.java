2a28dc2a7cc640b5d73b916d0144f43aa7c26e00
Eduardo Santos <eduardosantoshf@gmail.com>
2021-05-08 15:24:16 +0100

    Minor fix

--- AirQuality/src/main/java/airquality/controller/CityController.java
+++ AirQuality/src/main/java/airquality/controller/CityController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import airquality.repository.CityRepository;
 import airquality.service.CityService;
 
 import java.io.IOException;
@@ -14,10 +13,6 @@ import java.util.Map;
 
 @RestController
 public class CityController {
-
-    @Autowired
-    CityRepository cityRepository;
-
     @Autowired
     CityService cityService;
 
